75550844b22e908f415ae8f24a32ff0caaa44a5c
Prem Chand <premchand@technoart-studio.com>
2025-09-01 02:16:30 +0530

    [Release Commit]

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2137381850"
+    serviceWorkerVersion: "68693269"
   }
 });
